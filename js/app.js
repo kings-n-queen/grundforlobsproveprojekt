9e7611fbda4f0854ebec6c86490324bb52f8e37a
IsabellaRobinson <134569@rts-365.dk>
2020-09-03 10:27:53 +0200

    Header med logo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-import listevisning from "./listevisning";
+// import listevisning from "./listevisning";
+
+let burgerKnap = document.querySelector(".burger-knap");
+let myNavbar = document.getElementById("myNavbar");
+
+burgerKnap.addEventListener("click", function(){
+  console.log("Virker det overhovedet?")
+  myNavbar.classList.toggle("js-hidden");
+})
