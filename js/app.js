2245ee7e1c2010cb1944cb9bf102e69f44865188
IsabellaRobinson <134569@rts-365.dk>
2020-09-03 10:45:56 +0200

    header ret

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// import listevisning from "./listevisning";
-
-let burgerKnap = document.querySelector(".burger-knap");
-let myNavbar = document.getElementById("myNavbar");
-
-burgerKnap.addEventListener("click", function(){
-  console.log("Virker det overhovedet?")
-  myNavbar.classList.toggle("js-hidden");
-})
